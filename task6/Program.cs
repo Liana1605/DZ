b4fcff9260db8628b3304f2f518e00aaaebc41d6
liana16 <liana_you@bk.ru>
2022-11-25 16:29:51 +0300

    Задача 6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите одно число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA %2 == 0 & numberA > 0) 
+{
+    Console.WriteLine("Да"); 
+}
+else
+{
+    Console.WriteLine("Нет");
+}
