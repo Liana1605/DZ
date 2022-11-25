db125ddba04e088664b10a9fd239037b14205b91
liana16 <liana_you@bk.ru>
2022-11-25 18:25:22 +0300

    Задача 8

---
+++ task8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите одно число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if(numberN > 1)
+{
+    int numberS = 2;
+    while(numberS <= numberN)
+    {
+        Console.Write($"{numberS} ");
+        numberS += 2;
+    }
+}
+else if(numberN < 1)
+{
+   int numberS = 0;
+   while(numberS >= numberN)
+   {
+        Console.Write($"numberS ");
+        numberS -= 2;
+   }
+}
+else
+{
+    Console.WriteLine("ошибка");
+}
